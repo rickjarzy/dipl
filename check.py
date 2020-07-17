2a80461c0ba24d0974249238f9dceb9e23a5a69a
Paul <peru77@gmx.at>
2020-07-17 21:35:05 +0200

    added check

---
+++ check.py
@@ -0,0 +1,34 @@
+import torch
+import numpy
+
+
+if __name__ == "__main__":
+
+    A = torch.ones(15,3)
+    data = torch.rand(15,2400,2400)
+    qual = torch.rand(15,2400,2400)
+
+    torch.arange(1,16,1, out=A[:,1])
+    torch.arange(1,16,1, out=A[:, 2])
+    A[:, 2] = A[: ,2]**2
+
+
+    data = torch.reshape(data, (15,2400*2400))
+    qual = torch.reshape(qual, (15, 2400 * 2400))
+
+    qual_np = numpy.random.rand(15,2400,2400)
+    qual_np = qual_np.reshape(15,2400*2400)
+    qual_np = numpy.where(qual_np>0.5,1, qual_np)
+
+    data[(data>=0.5 & data <= 0.7)] = 1
+
+    print("numpy qual")
+    print(qual_np)
+
+    print("data")
+    print(data)
+
+    print("Programm ENDE")
+
+
+
