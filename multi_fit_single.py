ae005006b98b2185fed14626eb8efb6883c64eaf
Arzberger Paul <Paul.Arzberger@xal.com>
2020-09-07 14:36:23 +0200

    added iotcluster name to path info

--- multi_fit_single.py
+++ multi_fit_single.py
@@ -36,6 +36,12 @@ if __name__ == "__main__":
         in_dir_qs = r"E:\MODIS_Data\v6\tiff_single\MCD43A2"
         in_dir_tf = r"E:\MODIS_Data\v6\tiff_single\MCD43A4"
         out_dir_fit = r"E:\MODIS_Data\v6\fitted"
+
+    elif socket.gethostname() == "iotmaster":
+        in_dir_qs = r"/media/paul/Daten_Diplomarbeit/MODIS_Data/v6/tiff_single/MCD43A2"
+        in_dir_tf = r"/media/paul/Daten_Diplomarbeit/MODIS_Data/v6/tiff_single/MCD43A4"
+        out_dir_fit = r"/media/paul/Daten_Diplomarbeit/MODIS_Data/v6"
+
     else:
         print("Check Data Drive Letter !!!!!")
 
