11dd81b8250c2dd54bebb11638c359a4b8527217
Arzberger Paul <Paul.Arzberger@xal.com>
2021-03-07 18:47:24 +0100

    added TS plot

--- MODISTSPlots3.py
+++ MODISTSPlots3.py
@@ -21,8 +21,8 @@
 # TS
 # ========================================
 
-# todo: select a year by index
-# todo: select different calculations from fit
+# todo: select a year by index - done
+# todo: select different calculations from fit - done
 # todo: select corresponding raw data for same time periode
 # todo: create first plot with
 
@@ -32,16 +32,167 @@ import time
 import socket
 import numpy
 
-from fit_information import fit_info_all, fit_info_poly, fit_info_fft
+from osgeo import gdal, ogr
+from matplotlib import pyplot as plt
+from fit_information import fit_info_all, fit_info_poly, fit_info_fft, doy_factors
+
+def read_out_modis_values(epochs_path_list, shp_koords_list, root_dir=False):
+    """
+    Extracts the rastervalue of the MODIS image according to its position. the x and y shape koordinates are taken an
+    with the resolution and extend coordinates of the image, the corresponding row and cols for the underlying pixel value
+    are calculated
+    Parameters
+    ----------
+    epochs_path_list - list - with the epochs file names
+    shp_koords_list  - list touple -  holding the x and y koords of the shape point
+    root_dir         - string - if you need to fully reconstruct the path to the data
+
+    Returns          - list - with all pixel data from the sat epochs from the epochs_path_list
+    -------
+
+    """
+
+    print("\nREAD OUT SAT DATA\n====================")
+    print("shp koords: ", shp_koords_list)
+    print("Epoch to read out: ", epochs_path_list)
+
+    shp_x = shp_koords_list[0]
+    shp_y = shp_koords_list[1]
+
+    return_ras_data_list = []
+
+    for epoch in epochs_path_list:
+        if root_dir:
+            epoch_path = os.path.join(root_dir, epoch)
+        else:
+            epoch_path = epoch
+
+        #print("Processing epoch: ", epoch_path)
+
+        # read out the reference system infos
+        ras = gdal.Open(epoch_path, gdal.GA_ReadOnly)
+        ras_band = ras.GetRasterBand(1)                 # interesstingly, here one has to count starting from one, also if only one raster band is available
+        ras_geo_ref = ras.GetGeoTransform()
+
+        # print("raster bands nr: ", ras.RasterCount)
+        #print("ras_geo_ref: ", ras_geo_ref)
+
+        ras_xorigin = ras_geo_ref[0]
+        ras_yorigin = ras_geo_ref[3]
+        ras_res = ras_geo_ref[1]
+        ras_cols = ras.RasterXSize
+        ras_rows = ras.RasterYSize
+
+        ras_x_ul = ras_xorigin
+        ras_y_ul = ras_yorigin
+
+        #calcuate the satellite image extend
+        ras_x_lr = ras_xorigin + ras_res * ras_cols
+        ras_y_lr = ras_yorigin + ras_geo_ref[5] * ras_rows          # negative ras_resolution (0.0, 463.31271652750013, 0.0, 5559752.597460333, 0.0, -463.31271652750013)
+
+        #print("Sat Extend UL: ",(ras_x_ul, ras_y_ul))
+        #print("Sat Extend LR: ", (ras_x_lr, ras_y_lr))
+
+        if shp_x >= ras_x_ul or shp_x <= ras_x_lr:
+            if shp_y <= ras_y_ul and shp_y >= ras_y_lr:
+                print("Shp Coords in SatExtend")
+
+                # calculate the pixel position in  the image matrix that was marked with the shape geometry
+                ras_x_ind_BildMatrix = int(abs((abs(ras_xorigin) - abs(shp_x)) / ras_res))
+                ras_y_ind_BildMatrix = int(abs((abs(ras_yorigin) - abs(shp_y)) / ras_res))
+
+                # reading out the sat data has a return like --> array([[370]], dtype=int16) so we have to access that nested array with indizes [0][0]
+                ras_data = ras_band.ReadAsArray(ras_x_ind_BildMatrix, ras_y_ind_BildMatrix, 1, 1)[0][0]
+
+                if ras_data <= 0:
+                    ras_data = 0
+
+
+            else:
+                print("Shp y Coordinate not ins SatSzene Extent")
+                ras_data = 0
+        else:
+            print("Shp x Coordinate not ins SatSzene Extent")
+            ras_data = 0
+
+        # appending a single value to the return list
+        return_ras_data_list.append(ras_data)
+
+    return return_ras_data_list
+
+
+def read_out_shp_koord(shp_path):
+    """
+    Read out the X and Y Koords from each Point Shape Polygon from the handed Shape Path
+    Parameters
+    ----------
+    shp_path - string - holds the path to the shape file
+
+    Returns - dictionary - holding key value pairs for  "koords":[Xkoordinate of the shape, Ykoordinate of the shape]
+                                                        "desc": Where is the shape point location - woods, see, beach etc
+    -------
+
+    """
+    print("Extract X/Y Koords from Shape file")
+
+    shp_poi_koords = []
+    return_dict = {}
+    shpDrv = ogr.GetDriverByName('ESRI Shapefile')
+    dsPoi = shpDrv.Open(shp_path)
+    lyr = dsPoi.GetLayer()
+    numPoi = lyr.GetFeatureCount()
+    print("number of points: ", numPoi)
+    # iterate through Features
+    for ft in range(0, numPoi, 1):
+        actPoi = lyr.GetFeature(ft)
+        shpPoiGeom = actPoi.GetGeometryRef()
+        xPoi = shpPoiGeom.GetX()
+        yPoi = shpPoiGeom.GetY()
+
+        shp_poi_koords.append([xPoi, yPoi])
+        return_dict[ft] = {"koords":[xPoi, yPoi], "desc":actPoi.GetField("Desc")}
+
+    return return_dict
+
+
+def plot_ts_with_shape(input_dict):
+
+    x_axe_data = numpy.array(range(0,46,1))
+
+
+
+    print("Start plotting ...")
+    for shape_id in input_dict.keys():
+        plotted_year = input_dict[shape_id]["year"]
+        print("Create Plot for %d and  shape nr %d: "%(plotted_year, shape_id))
+        print("ID Desc : ", input_dict[shape_id].keys())
+
+        location_desc = input_dict[shape_id]["desc"]
+        print("Fit Products: ", input_dict[shape_id]["fit_products"].keys())
+
+        fig, ax = plt.subplots()
+        ax.set_title("FFT Comparison - %s" % location_desc)
+        # iterate through the fit products and create for each shape id a plot and show it
+        for fit_product in input_dict[shape_id]["fit_products"].keys():
+
+            print("processing fit product: ", fit_product)
+            print(input_dict[shape_id]["fit_products"][fit_product].keys())
+            print("data: ", input_dict[shape_id]["fit_products"][fit_product]["fit_data"])
+
+            data_array = input_dict[shape_id]["fit_products"][fit_product]["fit_data"]
+
+            ax.plot(x_axe_data,data_array, label=fit_product)
+
+        ax.set_xlabel("Day Of Year in %s" % plotted_year)
+        ax.set_ylabel("Reflexion [%]")
+        plt.legend()
+        plt.show()
 
 
 
 def main():
 
     print("Start Processing Plots for yearwise timeseries")
-
-if __name__ == "__main__":
-
     # 2T USB3 Festplatte und Home Rechner
     if socket.gethostname() in ['XH-AT-NB-108', 'Paul-PC']:
         in_dir_qs = r"E:\MODIS_Data\v6\tiff_single\MCD43A2"
@@ -53,30 +204,37 @@ if __name__ == "__main__":
 
     if os.name == "posix" and socket.gethostname() == "paul-buero":
 
-        in_dir_qs = r"/media/paul/Daten_Diplomarbeit2/MODIS_Data/v6/tiff_single/MCD43A2"
-        in_dir_tf = r"/media/paul/Daten_Diplomarbeit2/MODIS_Data/v6/tiff_single/MCD43A4"
+        in_dir_qs =   r"/media/paul/Daten_Diplomarbeit2/MODIS_Data/v6/tiff_single/MCD43A2"
+        in_dir_tf =   r"/media/paul/Daten_Diplomarbeit2/MODIS_Data/v6/tiff_single/MCD43A4"
         out_dir_fit = r"/media/paul/Daten_Diplomarbeit2/MODIS_Data/v6/fitted"
 
     elif os.name == "posix" and socket.gethostname() in ["iotmaster", "iotslave1", "iotslave2"]:
-        in_dir_qs = r"/home/iot/scripts/dev/projects/timeseries/data/v6/tiff_single/MCD43A2"
-        in_dir_tf = r"/home/iot/scripts/dev/projects/timeseries/data/v6/tiff_single/MCD43A4"
+        in_dir_qs =   r"/home/iot/scripts/dev/projects/timeseries/data/v6/tiff_single/MCD43A2"
+        in_dir_tf =   r"/home/iot/scripts/dev/projects/timeseries/data/v6/tiff_single/MCD43A4"
         out_dir_fit = r"/home/iot/scripts/dev/projects/timeseries/data/v6/fitted"
     else:
-        in_dir_qs = r"E:\MODIS_Data\v6\tiff_single\MCD43A2"
-        in_dir_tf = r"E:\MODIS_Data\v6\tiff_single\MCD43A4"
+        in_dir_qs =   r"E:\MODIS_Data\v6\tiff_single\MCD43A2"
+        in_dir_tf =   r"E:\MODIS_Data\v6\tiff_single\MCD43A4"
         out_dir_fit = r"E:\MODIS_Data\v6\fitted"
-
+        shp_dir =     r"E:\MODIS_Data\shp\checkFitPlots"
 
     tile = "h18v04"
+
+    shps = os.chdir(shp_dir)
+
+    shp_file = glob.glob("checkFitProducts1804.shp")[0]
+    shp_info = read_out_shp_koord(os.path.join(shp_dir,shp_file))
+
+    print("Found ShapeFile: ", shp_file)
+    print("SHP Info Dict: ", shp_info)
 
     sat_raw_indir = in_dir_tf
     sat_qual_indir = in_dir_qs
     sat_fitted_indir = out_dir_fit
 
-
-    doy_full = [doy for doy in range(1,365,8)]
-    doy_57 = [doy for doy in range(57,365,8)]
-    doy_113 = [doy for doy in range(113,365,8)]
+    doy_full = [doy for doy in range(1, 365, 8)]
+    doy_57 = [doy for doy in range(57, 365, 8)]
+    doy_113 = [doy for doy in range(113, 365, 8)]
 
     nr_msing_doy_indizes = doy_full[0:min(doy_57)]
 
@@ -85,28 +243,86 @@ if __name__ == "__main__":
     print("doy_113:\t", len(doy_113))
     print("doy_msing:\t", len(nr_msing_doy_indizes))
 
-    user_year = 2004
+    user_year = 2002
+    # calcute the starting point of the year defined by user_year
+    ts_raw_base_index = len(doy_57) + len(doy_full) * doy_factors[user_year]["factor"]  # this is the index where the year 2001 epoch starts in the data_lists for the bands
 
-    ts_raw_base_index = len(doy_57)           # this is the index where the year 2001 epoch starts in the data_lists for the bands
+    # calculate the end point of the year defined by the user_year
     ts_raw_end_index = ts_raw_base_index + len(doy_full)
 
-    ts_fit_base_indes = len(doy_113)*
-    ts_fit_end_index = ts_fit_base_indes + len(doy_full)
-
+    #
+    ts_fit_base_index = len(doy_113) + len(doy_full) * doy_factors[user_year]["factor"]
+    ts_fit_end_index = ts_fit_base_index + len(doy_full)
+    print("ts_raw_base_index: ", ts_raw_base_index)
+    print("ts_raw_end_index : ", ts_raw_end_index)
+    print("ts_fit_base_index: ", ts_fit_base_index)
+    print("ts_fit_end_index:  ", ts_fit_end_index)
+    print()
     # get into raw dir and select year001
     os.chdir(os.path.join(in_dir_tf, tile))
     raw_data_list_band1 = sorted(glob.glob("*.band_1.tif"))
 
     # get into  fit dir and select year001
-    os.chdir(os.path.join(out_dir_fit,tile))
+    os.chdir(os.path.join(out_dir_fit, tile))
 
+    print("Startung point raw raster epoch: ", raw_data_list_band1[ts_raw_base_index])
+    print("Ending point raw raster epoch:   ", raw_data_list_band1[ts_raw_end_index])
 
-    for fit_product in fit_info_fft.keys():
+    print(raw_data_list_band1[ts_raw_base_index:ts_raw_end_index])
+
+    # store the specific year data onto the fit_info_XXX Dict. It holds on the key files_list a list with tif epochs for the fit product of the
+    # defined year . eg. 2005 --> 2005001 - 2005361
+
+    used_fit_info_dict = fit_info_fft
+    #used_fit_info_dict = fit_info_poly
+
+    # write the epochs file names onto the dict
+    for fit_product in used_fit_info_dict.keys():
         print("Fit Product: ", fit_product)
-        fit_info_fft[fit_product]["files_list"] = sorted(glob.glob("*.band_1.sg_15_fft_poly.tif"))
+        used_fit_info_dict[fit_product]["root_dir"] = os.path.join(out_dir_fit, tile)
 
-    print("len band_1 ras list: ", len(raw_data_list_band1))
-    print("Raster RAW 2001001: ", raw_data_list_band1[ts_raw_base_index])
-    print("Raster Fit 2001001: ", fit_data_list_band1[ts_fit_base_indes] )
+        # fft files start with year 2001-001!!!!!
+        if fit_product == "fft":
+            ts_fit_base_index = len(doy_full) * doy_factors[user_year]["factor"]
+            ts_fit_end_index = ts_fit_base_index + len(doy_full)
+
+        used_fit_info_dict[fit_product]["files_list"] = sorted(glob.glob("*.band_1.%s.tif" % fit_product))[
+                                                        ts_fit_base_index:ts_fit_end_index]
+
+        print("Starting point fit raster epoch: ", used_fit_info_dict[fit_product]["files_list"][0])
+        print("Endiing point fit raster epoch : ", used_fit_info_dict[fit_product]["files_list"][-1])
+
+
+    # read out the satellite data on the specific shape koordinates and attach it to the dict
+    for shp_index in shp_info.keys():
+        shp_info[shp_index].update({"fit_products":used_fit_info_dict})
+        koords_to_process = shp_info[shp_index]["koords"]
+
+        for fit_product in shp_info[shp_index]["fit_products"].keys():
+
+            #print(fit_product, " len of epochs: ", len(shp_info[shp_index]["fit_products"][fit_product]["files_list"]))
+
+            shp_info[shp_index]["fit_products"][fit_product].update({"fit_data": read_out_modis_values(shp_info[shp_index]["fit_products"][fit_product]["files_list"],
+                                                   koords_to_process,
+                                                   root_dir=shp_info[shp_index]["fit_products"][fit_product]["root_dir"])})
+
+            #print(shp_info[shp_index]["fit_products"][fit_product]["fit_data"])
+
+        shp_info[shp_index].update({"year": user_year})
+
+    # todo: raw data an das shp_info anhängen
+
+    # create a plot for the raster data TS
+
+    plot_ts_with_shape(shp_info)
+
+
+
+if __name__ == "__main__":
+
+
+    main()
+
+
 
     print("Programm ENDE")
--- fit_information.py
+++ fit_information.py
@@ -12,7 +12,9 @@ fitted_products_list = ["double_fft_sg_15",
 fit_info_all = {}
 fit_info_poly = {}
 fit_info_fft = {}
-
+doy_factors = {}
+# All fits together in one dict
+# ----------------------------------------------------------------------------------------------------------------------
 fit_info_all["double_fft_sg_15"] = {"algo_desc": "calculates twice a FFT using a SQ window of 15 days", "weights":"no", "files_list":[]}
 fit_info_all["single_fft_sg_15"] = {"algo_desc": "calculates a single FFT over a SG window of 15 days", "weights":"no", "files_list":[]}
 fit_info_all["fft_sg_15"] = {"algo_desc": " calculates a FFT over a SG window of 15 days", "weights":"no", "files_list":[]}
@@ -21,31 +23,41 @@ fit_info_all["sg_15_fft_poly"] = {"algo_desc":"calculates a FFT first and then c
 fit_info_all["fft"] = {"algo_desc":"calculates a single FFT over an entire year - 46 epochs - recalculates the values via FFT and writes out each file of the year new", "weights":"no", "files_list":[]}
 
 
-
-fit_info_all["lin_poly_win15.weights.1_0.5_0.01_0.01"] = {"algo_desc": "calculates first a linear regresion on all NaN Pixels and then perfomrs twice a Poly Fit with "
-                                                                             "updateing the weights", "weights":"yes", "files_list":[]}
+# i have only 3 fitted epochs - i think this is because they have exact the same results as poly_lin_win15.
+# fit_info_all["lin_poly_win15.weights.1_0.5_0.01_0.01"] = {"algo_desc": "calculates first a linear regresion on all NaN Pixels and then perfomrs twice a Poly Fit with "
+#                                                                              "updateing the weights", "weights":"yes", "files_list":[]}
 fit_info_all["lin_poly_win15.weights.1_0.5_0.01_0.01_q0.01"] = {"algo_desc": "calculates first a linear regresion on all NaN Pixels and then perfomrs twice a Poly Fit with "
                                                                              "updateing the weights", "weights":"yes", "files_list":[]}
 fit_info_all["poly_15.1_0.01_0.01_0.01"] = {"algo_desc": "calculates a poly fit with a SG window of 15 and no lin interpolation", "weights":"yes", "files_list":[]}
 fit_info_all["poly_lin_win15.weights.1_0.5_0.01_0.01"] = {"algo_desc":"calculates a double poly fit with a SG window first and then a lin interpolation"
                                                                 "on the pixels that had NaN values or less then a specific inversion number", "files_list":[]}
 
-
+# FFT FITs Only in this dict
+# ----------------------------------------------------------------------------------------------------------------------
 fit_info_fft["double_fft_sg_15"] = {"algo_desc": "calculates twice a FFT using a SQ window of 15 days", "weights":"no", "files_list":[]}
 fit_info_fft["single_fft_sg_15"] = {"algo_desc": "calculates a single FFT over a SG window of 15 days", "weights":"no", "files_list":[]}
-fit_info_fft["fft_sg_15"] = {"algo_desc": " calculates a FFT over a SG window of 15 days", "weights":"no", "files_list":[]}
 fit_info_fft["sg_15_fft_poly"] = {"algo_desc":"calculates a FFT first and then calculates a Poly Fit twice in a SG window of 15 days", "weights":"yes", "files_list":[]}
-
 fit_info_fft["fft"] = {"algo_desc":"calculates a single FFT over an entire year - 46 epochs - recalculates the values via FFT and writes out each file of the year new", "weights":"no", "files_list":[]}
 
+# POLY FITs Only in this dict
+# ----------------------------------------------------------------------------------------------------------------------
+# lin_poly_win15.weights.1_0.5_0.01_0.01 hat aktuell nur 3 epochen - weil ich glaub es ergaben sich di total gleichen werte wie in poly_lin
+# fit_info_poly["lin_poly_win15.weights.1_0.5_0.01_0.01"] = {"algo_desc": "calculates first a linear regresion on all NaN Pixels and then perfomrs twice a Poly Fit with "
+#                                                                              "updateing the weights", "weights":"yes", "files_list":[]}
 
-fit_info_poly["lin_poly_win15.weights.1_0.5_0.01_0.01"] = {"algo_desc": "calculates first a linear regresion on all NaN Pixels and then perfomrs twice a Poly Fit with "
-                                                                             "updateing the weights", "weights":"yes", "files_list":[]}
 fit_info_poly["lin_poly_win15.weights.1_0.5_0.01_0.01_q0.01"] = {"algo_desc": "calculates first a linear regresion on all NaN Pixels and then perfomrs twice a Poly Fit with "
                                                                              "updateing the weights", "weights":"yes", "files_list":[]}
+
 fit_info_poly["poly_15.1_0.01_0.01_0.01"] = {"algo_desc": "calculates a poly fit with a SG window of 15 and no lin interpolation", "weights":"yes", "files_list":[]}
 fit_info_poly["poly_lin_win15.weights.1_0.5_0.01_0.01"] = {"algo_desc":"calculates a double poly fit with a SG window first and then a lin interpolation"
                                                                 "on the pixels that had NaN values or less then a specific inversion number", "files_list":[]}
+
+# create the factor for the list index calculation in the main programm of the MODISPlots3.py
+# Start 2001 has factor 0
+# I dont have the full year of 2000 because the first epochs start with doy 57, so the plots start with year 2001
+for year in range(1,21,1):
+    doy_factors[2000+year] = {"factor":year-1}
+
 
 if __name__ == "__main__":
 
